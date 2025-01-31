d84212434e68c698f4e9d71656c016084b9182ab
mojo <igormalj@gmail.com>
2025-01-31 21:15:33 +0100

    reinit project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Routes, Route } from "react-router";
+
 const App = () => {
-  return <div className="text-gray-500">Archery Event Admin</div>;
+  return (
+    <div className="min-w-[100vw] min-h-[100vh]">
+      <Routes>
+        <Route path="/" element={"hello world"} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./config/queryClient.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <ReactQueryDevtools position="bottom" />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>
+);
