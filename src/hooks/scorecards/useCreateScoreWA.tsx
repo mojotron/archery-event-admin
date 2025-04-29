7df6855ecb746e445c25b955cdb7c1ce75b093e0
mojo <igormalj@gmail.com>
2025-04-29 19:59:11 +0200

    update hook

--- src/hooks/scorecards/useCreateScoreWA.tsx
+++ src/hooks/scorecards/useCreateScoreWA.tsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ScoreWAType } from "../../types/scorecardType";
 
 const useCreateScoreWA = (rounds: number) => {
   const [scoreWA, setScoreWA] = useState<ScoreWAType[]>(() =>
     Array.from({ length: rounds }, () => ({ first: 10, second: 10, third: 10 }))
   );
-  return { scoreWA };
+
+  const updateScore = useCallback(
+    (arrow: "first" | "second" | "third", index: number, hit: number) => {
+      setScoreWA((oldState) =>
+        oldState.map((score, i) =>
+          i === index ? { ...score, [arrow]: hit } : score
+        )
+      );
+    },
+    []
+  );
+
+  return { scoreWA, updateScore };
 };
 
 export default useCreateScoreWA;
